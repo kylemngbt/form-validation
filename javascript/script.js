99cd0cea4e2595e578f3d9ed8bcc8974af9a6fe2
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-12 15:31:41 +0800

    build: create the references for the spans

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+const nameError = document.querySelector("#name-error");
+const phoneError = document.querySelector("#phone-error");
+const emailError = document.querySelector("#email-error");
+const messageError = document.querySelector("#message-error");
+const submitError = document.querySelector("#submit-error");
