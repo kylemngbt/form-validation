b24bd9fd8fdaf4cb98cfa5412c1a365845b5d88c
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-12 15:43:07 +0800

    feat: define the validateName func

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,18 @@ const messageError = document.querySelector("#message-error");
 const submitError = document.querySelector("#submit-error");
 const contactName = document.querySelector("#contact-name");
 
+function validateName() {
+  let name = contactName.value;
 
+  if (name.length == 0) {
+    nameError.textContent = "Name is required.";
+  }
+  if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+    nameError.textContent = "Enter your full name."
+    return false
+  }
+  nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+  return true
+}
 
 contactName.addEventListener("keyup", validateName);
